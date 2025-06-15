abd8976f81b6ca6d27b3c47148e16c60c7940ec1
mathisbork <mathisborkowska@gmail.com>
2025-06-15 13:06:40 +0200

    ajout Erreur stock insufisant

---
+++ ErreurStockInsuffisant.hpp
@@ -0,0 +1,17 @@
+#ifndef ERREUR_STOCK_INSUFFISANT_HPP
+#define ERREUR_STOCK_INSUFFISANT_HPP#pragma once
+
+#include <stdexcept>
+#include <string>
+
+class ErreurStockInsuffisant : public std::runtime_error
+{
+private:
+
+
+public:
+
+
+};
+
+#endif
